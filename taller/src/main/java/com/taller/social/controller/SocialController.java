a7af2bd891662b3eafa7fce87298b4a9655f7f6a
Emiliano <Emiliano@Emiliano-PC>
2012-09-04 11:52:18 -0300

    commit inicial

---
+++ taller/src/main/java/com/taller/social/controller/SocialController.java
@@ -0,0 +1,40 @@
+package com.taller.social.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/social")
+public class SocialController {
+	@Autowired
+	private TwitterTemplate twitterTemplate;
+	
+	@RequestMapping(value = "/twitter/tweet", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Tweet> buscarTweets() {
+		return twitterTemplate.timelineOperations().getHomeTimeline();
+
+	}
+
+	@RequestMapping(value = "/twitter/perfil", method = RequestMethod.GET)
+	public @ResponseBody
+	TwitterProfile buscarPerfil() {
+		return twitterTemplate.userOperations().getUserProfile();
+	}
+
+	@RequestMapping(value = "/twitter/tweet/{mensaje}", method = RequestMethod.POST)
+	public @ResponseBody
+	Tweet crearTweet(@PathVariable String mensaje) {
+		return twitterTemplate.timelineOperations().updateStatus(mensaje);
+	}
+
+}
---
+++ taller/src/test/java/com/taller/social/controller/SocialControllerTest.java
@@ -0,0 +1,46 @@
+package com.taller.social.controller;
+
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-social-config.xml" })
+public class SocialControllerTest {
+	@Autowired
+	private SocialController socialController;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp(){
+		mockMvc = MockMvcBuilders.standaloneSetup(socialController).build();
+	}
+
+	@Test
+	public void buscarTweets_conConfiguracionCorrecta_retornaListaDeTweets() throws Exception {
+		mockMvc.perform(get("/social/twitter/tweet")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void buscarPerfil_existe_Usuario_retornaPerfilDelUsuario() throws Exception {
+		mockMvc.perform(get("/social/twitter/perfil")).andExpect(status().isOk());
+	}
+
+	@Ignore
+	@Test
+	public void crearTweet_conConfiguracionCorrecta_retornaTweet() throws Exception {
+		mockMvc.perform(post("/social/twitter/tweet")).andExpect(status().isCreated());
+	}
+	
+}
