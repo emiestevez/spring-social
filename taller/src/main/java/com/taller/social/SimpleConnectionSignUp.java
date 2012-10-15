124016e70c4ad848f9f134fc73fe549cbde3281d
Emiliano <Emiliano@Emiliano-PC>
2012-10-15 13:37:41 -0300

    Se agrega la posibilidad de que en el caso del usuario sea nuevo vaya a la pantalla de registracion.

--- taller/src/main/java/com/taller/social/SimpleConnectionSignUp.java
+++ taller/src/main/java/com/taller/social/SimpleConnectionSignUp.java
@@ -15,6 +15,11 @@
  */
 package com.taller.social;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.UserProfile;
@@ -23,14 +28,34 @@ import org.springframework.social.connect.UserProfile;
  * Simple little {@link ConnectionSignUp} command that allocates new userIds in
  * memory. Doesn't bother storing a user record in any local database, since
  * this quickstart just stores the user id in a cookie.
- *
+ * 
  * @author Keith Donald
  */
 public final class SimpleConnectionSignUp implements ConnectionSignUp {
+	@Autowired
+	@Qualifier(value = "listaDeUsuarios")
+	private ArrayList<String> arrayList;
 
-    public String execute(Connection<?> connection) {
-        UserProfile userProfile = connection.fetchUserProfile();
+	public String execute(Connection<?> connection) {
+		UserProfile userProfile = connection.fetchUserProfile();
 
-        return userProfile.getUsername();
-    }
+		System.out.println("Tamaño de la lista: " + arrayList.size());
+
+		if (arrayList.size() == 0) {
+			arrayList.add(userProfile.getUsername());
+			return null;
+		}
+
+		if (arrayList.contains(userProfile.getUsername())) {
+			return userProfile.getUsername();
+		} else {
+			arrayList.add(userProfile.getUsername());
+			return null;
+		}
+
+		// if (userProfile.getUsername().equals("epestevez")) {
+		// return null;
+		// }
+
+	}
 }
