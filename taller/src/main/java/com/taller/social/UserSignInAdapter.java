86e9b3fa213c1afcf0f5404ea62fd577a8b67217
Emiliano <Emiliano@Emiliano-PC>
2012-10-15 22:12:53 -0300

    Se elimina import que no se usa.

--- taller/src/main/java/com/taller/social/UserSignInAdapter.java
+++ taller/src/main/java/com/taller/social/UserSignInAdapter.java
@@ -5,7 +5,6 @@
 package com.taller.social;
 
 import org.springframework.social.connect.Connection;
-import org.springframework.social.connect.UserProfile;
 import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.NativeWebRequest;
